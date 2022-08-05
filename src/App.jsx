72d46dcd24e554001a427c1beabd83f08f95fa87
Zack Rogers <zack.rogers.t@gmail.com>
2022-08-05 16:34:06 -0600

    added users.js file for local db

--- src/App.jsx
+++ src/App.jsx
@@ -12,40 +12,7 @@ import AddAUser from "./screens/AddAUser";
 import IndividualUser from "./screens/IndividualUser";
 import NotFound from "./screens/NotFound";
 
-let users = [
-	{
-		id: 1,
-		name: "John Smith",
-		location: "Manhatten, New York",
-		title: "Full-Stack Web Developer",
-		employer: "Apple",
-		movies: ["Tron", "Martian"]
-	},
-	{
-		id: 2,
-		name: "Jack Ripper",
-		location: "Buffalo, New York",
-		title: "Front End Web Developer",
-		employer: "Apple",
-		movies: ["Narnia", "SAO"]
-	},
-	{
-		id: 3,
-		name: "James May",
-		location: "Queens, New York",
-		title: "Back End Web Developer",
-		employer: "Apple",
-		movies: ["Blade Runner", "Gundam"]
-	},
-	{
-		id: 4,
-		name: "Joey Blast",
-		location: "Brooklyn, New York",
-		title: "Data Analyst",
-		employer: "Apple",
-		movies: ["Harry Potter", "JonTronShow"]
-	}
-];
+import users from "./data/users";
 
 function App() {
 	return (
