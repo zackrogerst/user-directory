8442fbd07d5629a8f61f03361cf6e13f6afa5ee9
Zack Rogers <zack.rogers.t@gmail.com>
2022-08-08 15:04:07 -0600

    removed routing temporarily

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-// import { Fragment } from "react";
-// import Header from "./components/Header";
-
 import { useState } from "react";
+import { Fragment } from "react";
+import Header from "./components/Header";
 
-import { Routes, Route, Navigate } from "react-router-dom";
+// import { Routes, Route, Navigate } from "react-router-dom";
 
 import "./App.css";
 
-import Layout from "./components/Layout";
+// import Layout from "./components/Layout";
 
 import AllUsers from "./screens/AllUsers";
 import AddAUser from "./screens/AddAUser";
@@ -19,24 +18,24 @@ import db from "./data/db";
 function App() {
 	const [users, setUsers] = useState(db);
 	return (
-		<Layout>
-			<Routes>
-				<Route path="/" element={<Navigate replace to="/users" />} />
-				<Route path="/users" element={<AllUsers users={users} setUsers={setUsers} />} />
-				<Route path="/users:id" element={<IndividualUser users={users} setUsers={setUsers} />} />
-				<Route path="/add-user" element={<AddAUser users={users} setUsers={setUsers} />} />
-				<Route path="*" element={<NotFound />} />
-			</Routes>
-		</Layout>
-		// <Fragment>
-		// 	<Header />
-		// 	<main>
-		// 		<AllUsers users={users} setUsers={setUsers} />
-		// 		<AddAUser users={users} setUsers={setUsers} />
-		// 		<IndividualUser users={users} setUsers={setUsers} />
-		// 		<NotFound />
-		// 	</main>
-		// </Fragment>
+		// <Layout>
+		// 	<Routes>
+		// 		<Route path="/" element={<Navigate replace to="/users" />} />
+		// 		<Route path="/users" element={<AllUsers users={users} setUsers={setUsers} />} />
+		// 		<Route path="/users:id" element={<IndividualUser users={users} setUsers={setUsers} />} />
+		// 		<Route path="/add-user" element={<AddAUser users={users} setUsers={setUsers} />} />
+		// 		<Route path="*" element={<NotFound />} />
+		// 	</Routes>
+		// </Layout>
+		<Fragment>
+			<Header />
+			<main>
+				<AllUsers users={users} setUsers={setUsers} />
+				<AddAUser users={users} setUsers={setUsers} />
+				<IndividualUser users={users} setUsers={setUsers} />
+				<NotFound />
+			</main>
+		</Fragment>
 	);
 }
 
